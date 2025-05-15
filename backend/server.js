d063a20c50ef2485e14d3566221aa17880a0bedb
ray1710 <93635574+ray1710@users.noreply.github.com>
2025-05-14 23:00:18 -0400

    fixed some backend login/signup functionality

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,55 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-app.post("/newuser", async (req, res) => {
+/**
+ * New user
+ */
+app.post("/signup", async (req, res) => {
   const user = req.body;
 
+  let check;
+  //Check if User Already Exists
+  try {
+    check = await User.find({ username: user["username"] }).exec();
+  } catch (error) {
+    throw error;
+  }
+  console.log(check);
+  if (check != "") {
+    res.send("Account Already Exists");
+    return;
+  }
   const newUser = new User(user);
   try {
     await newUser.save();
     res.send("Sucess");
   } catch (error) {
     res.send("Failed");
+  }
+});
+
+/**
+ * Login currrent user
+ */
+app.post("/login", async (req, res) => {
+  const details = req.body;
+
+  try {
+    const user = await User.find(
+      { username: details["username"] },
+      "username password"
+    ).exec();
+    if (user == "") {
+      res.send("Login Failed");
+    } else if (
+      details["username"] == user[0]["username"] &&
+      details["password"] == user[0]["password"]
+    ) {
+      res.send("Login Sucessful");
+    } else {
+      res.send("Login Failed");
+    }
+  } catch (error) {
+    res.status(500);
   }
 });
