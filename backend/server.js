f0e7d581a8f1ee5a81a9d508f419d7cbef1a99dc
ray1710 <93635574+ray1710@users.noreply.github.com>
2025-07-17 18:03:11 -0400

    deleted comments

--- backend/server.js
+++ backend/server.js
@@ -105,8 +105,8 @@ export async function getAlbumsFromGenres(genre, token) {
 
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
-    [array[i], array[j]] = [array[j], array[i]]; // swap
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
 }
