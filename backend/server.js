c8104e02bfaa707294b5677a329602ae3de2b4ce
ray1710 <93635574+ray1710@users.noreply.github.com>
2025-07-18 12:28:45 -0400

    Revert "returns basic items from albums from backend"
    This reverts commit e1613a8f40dccf951bdaf2cc6522fc6a42c5d3c9.

--- backend/server.js
+++ backend/server.js
@@ -115,16 +115,8 @@ function getArtistsFromAlbums(arr) {
 }
 
 function getTracksFromAlbums(tracks) {
-  let res = [];
   console.log(tracks);
-  for (let i = 0; i < tracks.items.length; i++) {
-    res.push({
-      track_num: tracks.items[i].track_number,
-      track_name: tracks.items[i].name,
-      artist: getArtistsFromAlbums(tracks.items[i].artists),
-    });
-  }
-  return res;
+  return [];
 }
 const verifyToken = (req, res, next) => {
   const auth = req.headers.authorization;
